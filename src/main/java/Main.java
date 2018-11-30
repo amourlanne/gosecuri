c97cfc5592e9baaa11f508658cd4d093fe3898c7
Alexis Mourlanne <alexis.mourlanne@etu.u-bordeaux.fr>
2018-11-30 16:09:34 +0100

    changement du gitignore

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Class Main
+ * Created by Alexis on 30/11/2018
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("gosecuriti");
+    }
+}
