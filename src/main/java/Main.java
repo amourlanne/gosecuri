58c543d7aa383a424fb1e6b2a7efb63ef7d1a86c
Alexis Mourlanne <alexis.mourlanne@etu.u-bordeaux.fr>
2018-12-10 23:05:21 +0100

    Ajout du stream de camera en direct

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,14 @@
+import controller.HomeController;
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import org.opencv.core.Core;
 
 /**
  * Class Main
@@ -11,17 +17,36 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     public static void main(String[] args) {
+
+        // load the native OpenCV library
+        nu.pattern.OpenCV.loadShared();
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("templates/home.fxml"));
-        primaryStage.setTitle("GoSecuri");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.setX(780);
-        primaryStage.setY(50);
-        primaryStage.setResizable(false);
-        primaryStage.show();
+
+        try
+        {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("templates/home.fxml"));
+            Parent root = null;
+
+            root = loader.load();
+            HomeController controller = loader.getController();
+
+            primaryStage.setScene(new Scene(root));
+            primaryStage.setTitle("GoSecuri");
+            primaryStage.setX(780);
+            primaryStage.setY(50);
+            primaryStage.setResizable(false);
+            primaryStage.show();
+
+            primaryStage.setOnCloseRequest((we -> controller.setClosed()));
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
     }
 }
