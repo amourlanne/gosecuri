6f69e58ac82c9252fb33d7dc267e8834e7e96293
Alexis Mourlanne <alexis.mourlanne@etu.u-bordeaux.fr>
2018-12-10 18:06:34 +0100

    ajout de opencv

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,27 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  * Class Main
  * Created by Alexis on 30/11/2018
  */
-public class Main {
+public class Main extends Application {
 
     public static void main(String[] args) {
-        System.out.println("gosecuriti");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("templates/home.fxml"));
+        primaryStage.setTitle("GoSecuri");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setX(780);
+        primaryStage.setY(50);
+        primaryStage.setResizable(false);
+        primaryStage.show();
     }
 }
